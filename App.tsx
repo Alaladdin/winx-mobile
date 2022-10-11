5571a3a9057961a5cfc3ab9145098e2384c1c4e3
Aladdin <g.tabroid@gmail.com>
2022-10-11 03:56:18 +0300

    schedule page

--- App.tsx
+++ App.tsx
@@ -4,16 +4,20 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { MainNavigator } from './app/navigators';
 import theme from './app/theme';
+import Config from './app/config';
+import { ErrorBoundary } from './app/screens';
 
 export default function App() {
   library.add(fas);
 
   return (
     <PaperProvider theme={ theme }>
-      <StatusBar barStyle="light-content" backgroundColor={ theme.colors.background } />
-      <View style={ styles.container }>
-        <MainNavigator />
-      </View>
+      <ErrorBoundary catchErrors={ Config.catchErrors }>
+        <StatusBar barStyle="light-content" backgroundColor={ theme.colors.background } />
+        <View style={ styles.container }>
+          <MainNavigator />
+        </View>
+      </ErrorBoundary>
     </PaperProvider>
   );
 }
--- app/screens/ScheduleScreen.tsx
+++ app/screens/ScheduleScreen.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
-import { Text, ActivityIndicator, Card } from 'react-native-paper';
-import { map } from 'lodash';
+import { StyleSheet, View } from 'react-native';
+import { Text, ActivityIndicator, Card, Title } from 'react-native-paper';
+import { findIndex, groupBy, keys, map } from 'lodash';
+import moment from 'moment';
+import PagerView from 'react-native-pager-view';
 import { api } from '../services/api';
+import config from '../config';
 
 interface IScheduleItem {
   auditorium: string
@@ -18,45 +21,72 @@ interface IScheduleItem {
   lecturer: string,
 }
 
+const getDatesRanges = () => {
+  const offset = 20;
+  const start = moment().subtract(offset, 'days').format(config.serverDateFormat);
+  const end = moment().add(offset, 'days').format(config.serverDateFormat);
+
+  return [start, end];
+};
+
+const renderSchedule = (schedule: IScheduleItem, key) => (
+  <Card key={ key }>
+    <Card.Title
+      title={ schedule.disciplineAbbr }
+      left={ () => <Title>{ schedule.dayOfWeekString.toUpperCase() }</Title> }
+      right={ () => <Text>{ schedule.date }</Text> }
+    />
+    <Card.Content>
+      <Text>{ schedule.kindOfWork }</Text>
+      <Text>{ schedule.auditorium }</Text>
+    </Card.Content>
+  </Card>
+);
+
 export function ScheduleScreen() {
-  const [schedule, setSchedule] = useState(null);
+  const [start, finish] = getDatesRanges();
+  const [schedules, setSchedule] = useState(null);
   const [isLoading, setLoading] = useState(false);
+  const currentWeekStartDay = moment().add(2, 'days').startOf('isoWeek').format('DD_MM');
 
-  if (schedule === null && !isLoading) {
+  if (schedules === null && !isLoading) {
     setLoading(true);
-
-    api.get('/getSchedule')
+    api.get('/getSchedule', { start, finish })
       .then((data) => {
-        setSchedule(data.schedule);
-      })
-      .catch(() => {
-        // todo error
+        setSchedule(
+          groupBy(data.schedule, ({ date }: IScheduleItem) => moment(date, 'DD.MM').startOf('isoWeek').format('DD_MM'))
+        );
       })
       .finally(() => {
         setLoading(false);
       });
   }
 
+  if (isLoading || !schedules)
+    return <ActivityIndicator animating />;
+
+  const views = map(schedules, (weeklySchedules, index) => (
+    <View key={ index }>
+      { map(weeklySchedules, renderSchedule) }
+    </View>
+  ));
+
+  const initialIndex = findIndex(keys(schedules), (key) => key === currentWeekStartDay);
+
   return (
-    <ScrollView>
-      {
-        isLoading
-          ? <ActivityIndicator animating />
-          : (
-            map(schedule, (scheduleItem, index) => (
-              <Card key={ index }>
-                <Card.Content>
-                  <Card.Title
-                    title={ scheduleItem.disciplineAbbr }
-                    subtitle={ scheduleItem.kindOfWork }
-                    right={ () => <Text>{ scheduleItem.date }</Text> }
-                    left={ () => <Text>{ scheduleItem.dayOfWeekString.toUpperCase() }</Text> }
-                  />
-                </Card.Content>
-              </Card>
-            ))
-          )
-      }
-    </ScrollView>
+    <PagerView
+      style={ styles.pagerView }
+      initialPage={ initialIndex }
+      scrollEnabled
+    >
+      { views }
+    </PagerView>
   );
 }
+
+const styles = StyleSheet.create({
+  pagerView: {
+    width : '100%',
+    height: '100%',
+  },
+});
