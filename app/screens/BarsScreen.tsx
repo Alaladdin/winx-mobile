8accb68a32a0c7778d36e99586dc69e325589a03
Aladdin <g.tabroid@gmail.com>
2022-10-14 03:15:35 +0300

    Bars screen

--- app/screens/BarsScreen.tsx
+++ app/screens/BarsScreen.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import { Text } from 'react-native-paper';
-import { Route } from '../navigators/MainNavigator';
-// import { DemoTabScreenProps } from '../navigators/MainNavigator';
-// import { reportCrash } from '../utils/crash-reporting'
+import { ScrollView, StyleSheet } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import theme from '../theme';
 
-// _props: DemoTabScreenProps<'Bars'>
-export function BarsScreen({ route }: { route: Route }) {
-  // reportCrash(new Error('My first Sentry error!'))
+export function BarsScreen() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
-    <Text>{ route.title }</Text>
+    <ScrollView style={ styles.container } contentContainerStyle={ [styles.container, styles.containerContent] }>
+      <TextInput
+        style={ styles.input }
+        label="Login"
+        value={ username }
+        disabled
+        onChangeText={ setUsername }
+      />
+
+      <TextInput
+        style={ styles.input }
+        label="Password"
+        value={ password }
+        disabled
+        onChangeText={ setPassword }
+      />
+
+      <Button mode="elevated" disabled>
+        Login
+      </Button>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width    : '100%',
+    minHeight: '100%',
+  },
+  containerContent: {
+    flexDirection : 'column',
+    justifyContent: 'center',
+    padding       : theme.spacing.large,
+  },
+  input: {
+    marginBottom: theme.spacing.medium,
+  },
+});
