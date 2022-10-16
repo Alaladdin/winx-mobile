d53c51108fafa21ccf08f2e73e833ace9a95d722
Aladdin <g.tabroid@gmail.com>
2022-10-16 17:12:04 +0300

    refactor

--- app/screens/SettingsScreen/SettingsNotifications.tsx
+++ app/screens/SettingsScreen/SettingsNotifications.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { List, Text, Switch } from 'react-native-paper';
 import { View } from 'react-native';
 import { ISettingSection } from './ISettingSection';
 
 export function SettingsNotifications({ headingStyle }: ISettingSection) {
+  const disabledSwitch = useMemo(() => <Switch value={ false } disabled />, []);
+
   return (
     <View>
       <Text variant="headlineSmall" style={ headingStyle }>Notifications</Text>
 
       <List.Item
         title="About lessons"
-        right={ () => (<Switch value={ false } disabled />) }
+        right={ () => disabledSwitch }
       />
 
       <List.Item
         title="About bars marks change"
-        right={ () => (<Switch value={ false } disabled />) }
+        right={ () => disabledSwitch }
       />
     </View>
   );
