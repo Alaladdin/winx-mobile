4febbeee53b4c3a5c679b3caa52c7f37edc33343
Aladdin <g.tabroid@gmail.com>
2022-10-17 00:31:58 +0300

    utils, models, services

---
+++ app/utils/crash-reporting.ts
@@ -0,0 +1,29 @@
+import * as Sentry from 'sentry-expo';
+import Config from '../config';
+
+export const initCrashReporting = () => {
+  if (!__DEV__) {
+    Sentry.init({
+      dsn  : Config.sentryDsn,
+      debug: true,
+    });
+  }
+};
+
+export const reportCrash = (error: Error) => {
+  if (__DEV__) {
+    console.error(error);
+    console.tron.log(error);
+  } else {
+    Sentry.Native.captureException(error);
+  }
+};
+
+export const reportMessage = (message: any) => {
+  if (__DEV__) {
+    console.info(message);
+    console.tron.log(message);
+  } else {
+    Sentry.Native.captureMessage(message);
+  }
+};
