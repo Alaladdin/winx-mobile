f3cf0db83918cf5367217cb358927262e79c6af0
Aladdin <g.tabroid@gmail.com>
2022-11-06 03:07:20 +0300

    clear mainStore

--- app/models/MainStore.ts
+++ app/models/MainStore.ts
@@ -3,21 +3,14 @@ import { Instance, SnapshotOut, types } from 'mobx-state-tree';
 export const MainStoreModel = types
   .model('MainStore')
   .props({
-    _authToken: types.maybe(types.string),
     _hasUpdate: types.optional(types.boolean, false),
   })
   .views((store) => ({
-    get isAuthenticated() {
-      return !!store._authToken;
-    },
     get hasUpdates() {
       return !!store._hasUpdate;
     },
   }))
   .actions((store) => ({
-    setAuthToken(value?: string) {
-      store._authToken = value;
-    },
     setHasUpdate(value: boolean) {
       store._hasUpdate = !!value;
     },
