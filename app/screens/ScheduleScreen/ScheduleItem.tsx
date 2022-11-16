46936ca1ea8e2aaaf7249fb74ccae20e168d582d
Aladdin <g.tabroid@gmail.com>
2022-11-17 02:02:49 +0300

    Schedule screen

---
+++ app/screens/ScheduleScreen/ScheduleItem.tsx
@@ -0,0 +1,92 @@
+import { Surface, Text } from 'react-native-paper';
+import { StyleSheet, View, ViewStyle } from 'react-native';
+import { useMemo } from 'react';
+import theme from '@/theme';
+import { IScheduleItem } from '@/screens/ScheduleScreen/IScheduleItem';
+
+interface ScheduleItemProps {
+  schedule: IScheduleItem;
+  style?: ViewStyle
+}
+
+export function ScheduleItem({ schedule, style }: ScheduleItemProps) {
+  const cardHeaderStyles = useMemo(() => {
+    const stylesList: ViewStyle[] = [styles.cardHeader, styles.bgRed];
+
+    if (schedule.kindOfWorkId === 31)
+      stylesList.push(styles.bgYellow);
+
+    if (schedule.kindOfWorkId === 42)
+      stylesList.push(styles.bgGreen);
+
+    return stylesList;
+  }, [schedule.kindOfWorkId]);
+
+  return (
+    <View style={ style }>
+      <View style={ styles.cardBefore }>
+        <Text variant="titleSmall">
+          { `${schedule.beginLesson} – ${schedule.endLesson}` }
+        </Text>
+      </View>
+
+      <Surface style={ styles.card }>
+        <View style={ cardHeaderStyles }>
+          <Text variant="labelLarge">{ schedule.dayOfWeekString }</Text>
+          <Text variant="labelLarge">{ schedule.kindOfWork }</Text>
+          <Text variant="labelLarge">{ schedule.date }</Text>
+        </View>
+
+        <View style={ styles.cardBody }>
+          <View style={ { alignItems: 'center', width: '100%' } }>
+            <Text style={ { marginBottom: 10 } } variant="labelLarge">{ schedule.discipline }</Text>
+            <View style={ { flexDirection: 'row', alignItems: 'center', width: '100%' } }>
+              <Text style={ styles.cardBadge }>{ schedule.auditorium }</Text>
+              <Text style={ { color: theme.colors.onSurfaceDisabled, marginRight: 'auto' } }>{ schedule.lecturer }</Text>
+            </View>
+          </View>
+        </View>
+      </Surface>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardBefore: {
+    marginBottom: 10,
+  },
+  card: {
+    marginBottom: theme.spacing.extraLarge,
+    borderRadius: 20,
+    overflow    : 'hidden',
+  },
+  cardHeader: {
+    flexDirection    : 'row',
+    justifyContent   : 'space-between',
+    alignItems       : 'center',
+    paddingVertical  : 10,
+    paddingHorizontal: 20,
+  },
+  bgRed: {
+    backgroundColor: theme.colors.rose[500],
+  },
+  bgGreen: {
+    backgroundColor: theme.colors.green[500],
+  },
+  bgYellow: {
+    backgroundColor: theme.colors.orange[400],
+  },
+  cardBody: {
+    paddingVertical  : 15,
+    paddingHorizontal: 20,
+    flexDirection    : 'row',
+    alignItems       : 'center',
+  },
+  cardBadge: {
+    marginRight    : 'auto',
+    padding        : 10,
+    borderRadius   : 15,
+    fontSize       : 10,
+    backgroundColor: theme.colors.neutralVariant30,
+  },
+});
--- app/screens/ScheduleScreen/ScheduleScreen.tsx
+++ app/screens/ScheduleScreen/ScheduleScreen.tsx
@@ -1,99 +1,77 @@
 import { useEffect, useMemo, useRef } from 'react';
-import { View, ScrollView, StyleSheet, RefreshControl } from 'react-native';
-import { Text, Surface } from 'react-native-paper';
+import { ScrollView, StyleSheet, RefreshControl } from 'react-native';
 import { each, groupBy, map, some } from 'lodash';
 import moment from 'moment';
 import PagerView from 'react-native-pager-view';
 import { useQuery } from '@tanstack/react-query';
-import { api } from '@/services/api';
 import config from '@/config';
-import theme from '@/theme';
 import { Loader } from '@/components';
 import { IScheduleItem } from './IScheduleItem';
 import { EmptyState } from '@/components/EmptyState';
+import { useRequest, IRequestConfig } from '@/hooks/useRequest';
+import { ScheduleEmptyItem } from '@/screens/ScheduleScreen/ScheduleEmptyItem';
+import { ScheduleItem } from '@/screens/ScheduleScreen/ScheduleItem';
 
 const loaderScreen = <Loader />;
 const todayCompare = moment().format(config.serverDateFormat);
-const isDayBeforeToday = (one) => todayCompare > moment(one, config.defaultDateFormat).format(config.serverDateFormat);
+const isDayBeforeToday = (date) => todayCompare > moment(date, config.defaultDateFormat).format(config.serverDateFormat);
+const renderSchedule = (schedule: IScheduleItem, key) => {
+  if (!schedule.isEmpty) {
+    const cardStyle = isDayBeforeToday(schedule.date) && styles.cardDisabled;
 
-const renderSchedule = (schedule: IScheduleItem, key) => (
-  <View key={ key } style={ [isDayBeforeToday(schedule.date) && styles.cardDisabled] }>
-    {
-      schedule.isEmpty
-        ? (
-          <Surface style={ [styles.card, styles.cardEmpty] }>
-            <View style={ { justifyContent: 'center', alignItems: 'center', height: '100%' } }>
-              <Text style={ { color: theme.colors.neutralVariant50 } }>No lessons</Text>
-            </View>
-          </Surface>
-        ) : (
-          <>
-            <View style={ styles.cardBefore }>
-              <Text variant="titleSmall">
-                { `${schedule.beginLesson} – ${schedule.endLesson}` }
-              </Text>
-            </View>
+    return (
+      <ScheduleItem
+        key={ key }
+        schedule={ schedule }
+        style={ cardStyle }
+      />
+    );
+  }
 
-            <Surface style={ styles.card }>
-              <View style={ [
-                styles.cardHeader,
-                styles.cardRed,
-                schedule.kindOfWorkId === 31 && styles.cardYellow,
-                schedule.kindOfWorkId === 42 && styles.cardGreen,
-              ] }
-              >
-                <Text variant="labelLarge">{ schedule.dayOfWeekString }</Text>
-                <Text variant="labelLarge">{ schedule.kindOfWork }</Text>
-                <Text variant="labelLarge">{ schedule.date }</Text>
-              </View>
+  return (
+    <ScheduleEmptyItem key={ key } />
+  );
+};
 
-              <View style={ styles.cardContainer }>
-                <View style={ { alignItems: 'center', width: '100%' } }>
-                  <Text style={ { marginBottom: 10 } } variant="labelLarge">{ schedule.discipline }</Text>
-                  <View style={ { flexDirection: 'row', alignItems: 'center', width: '100%' } }>
-                    <Text style={ styles.cardBadge }>{ schedule.auditorium }</Text>
-                    <Text style={ { color: theme.colors.onSurfaceDisabled, marginRight: 'auto' } }>{ schedule.lecturer }</Text>
-                  </View>
-                </View>
-              </View>
-            </Surface>
-          </>
-        )
-   }
-  </View>
-);
-
-const loadSchedule = () => {
+const formatScheduleData = (data) => {
   const weekDays = ['пн', 'вт', 'ср', 'чт', 'пт'];
+  const formattedSchedules = map(data.schedule, (i) => ({ ...i, dayOfWeekString: i.dayOfWeekString.toLowerCase() }));
+  const groupedSchedules = groupBy(
+    formattedSchedules,
+    ({ date }) => moment(date, config.defaultDateFormat)
+      .startOf('isoWeek')
+      .format('MM_DD')
+  );
+
+  each(groupedSchedules, (weeklySchedules) => {
+    each(weekDays, (weekDay, i) => {
+      if (!some(weeklySchedules, { dayOfWeekString: weekDay }))
+        weeklySchedules.splice(i, 0, { isEmpty: true });
+    });
+  });
+
+  return groupedSchedules;
+};
+
+const getScheduleRequestOptions = (): IRequestConfig => {
   const start = moment().add(2, 'days').startOf('isoWeek').format(config.serverDateFormat);
   const finish = moment().add(2, 'months').endOf('isoWeek').format(config.serverDateFormat);
 
-  return api.get('/getSchedule', { start, finish })
-    .then((data) => {
-      const formattedSchedules = map(data.schedule, (i) => ({ ...i, dayOfWeekString: i.dayOfWeekString.toLowerCase() }));
-      const groupedSchedules = groupBy(
-        formattedSchedules,
-        ({ date }) => moment(date, config.defaultDateFormat)
-          .startOf('isoWeek')
-          .format('MM_DD')
-      );
-
-      each(groupedSchedules, (weeklySchedules) => {
-        each(weekDays, (weekDay, i) => {
-          if (!some(weeklySchedules, { dayOfWeekString: weekDay }))
-            weeklySchedules.splice(i, 0, { isEmpty: true });
-        });
-      });
-
-      return groupedSchedules;
-    });
+  return {
+    method       : 'get',
+    url          : '/getSchedule',
+    params       : { start, finish },
+    afterResponse: formatScheduleData,
+  };
 };
 
 export function ScheduleScreen({ navigation }): JSX.Element {
   const pagerViewRef = useRef<PagerView>(null);
-  const { data, refetch, isLoading, isRefetching, isError, isRefetchError } = useQuery(['schedule'], loadSchedule);
+  const loadSchedule = useRequest(getScheduleRequestOptions());
+  const { data, refetch, isLoading, isRefetching, isError } = useQuery(['schedule'], loadSchedule);
   const showLoader = useMemo(() => isLoading || isRefetching, [isLoading, isRefetching]);
-  const showEmptyState = useMemo(() => !data || isError || isRefetchError, [data, isError, isRefetchError]);
+  const showEmptyState = useMemo(() => !data || isError, [data, isError]);
+  const refresher = <RefreshControl refreshing={ isRefetching } onRefresh={ refetch } />;
 
   useEffect(() => navigation.addListener('tabPress', () => {
     pagerViewRef?.current?.setPage(0);
@@ -115,7 +93,7 @@ export function ScheduleScreen({ navigation }): JSX.Element {
       {
         map(data, (weeklySchedules, index) => (
           <ScrollView
-            refreshControl={ <RefreshControl refreshing={ isRefetching } onRefresh={ refetch } /> }
+            refreshControl={ refresher }
             contentContainerStyle={ { padding: 20 } }
             key={ index }
           >
@@ -132,57 +110,7 @@ const styles = StyleSheet.create({
     width : '100%',
     height: '100%',
   },
-  cardEmpty: {
-    borderStyle : 'dashed',
-    borderColor : theme.colors.neutralVariant50,
-    borderWidth : 1,
-    height      : 135,
-    borderRadius: 20,
-    marginBottom: 20,
-  },
-  cardBefore: {
-    marginBottom: 10,
-  },
-  card: {
-    marginBottom: 30,
-    borderRadius: 20,
-    overflow    : 'hidden',
-  },
   cardDisabled: {
     opacity: 0.4,
-  },
-  cardOver: {
-    height : '100%',
-    opacity: 0.5,
-    width  : '100%',
-  },
-  cardHeader: {
-    flexDirection    : 'row',
-    justifyContent   : 'space-between',
-    alignItems       : 'center',
-    paddingVertical  : 10,
-    paddingHorizontal: 20,
-  },
-  cardRed: {
-    backgroundColor: theme.colors.rose[500],
-  },
-  cardGreen: {
-    backgroundColor: theme.colors.green[500],
-  },
-  cardYellow: {
-    backgroundColor: theme.colors.orange[400],
-  },
-  cardContainer: {
-    paddingVertical  : 15,
-    paddingHorizontal: 20,
-    flexDirection    : 'row',
-    alignItems       : 'center',
-  },
-  cardBadge: {
-    marginRight    : 'auto',
-    padding        : 10,
-    borderRadius   : 15,
-    fontSize       : 10,
-    backgroundColor: theme.colors.neutralVariant30,
   },
 });
