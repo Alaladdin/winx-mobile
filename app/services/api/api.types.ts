ba118ac7976a110a032d26446997617d8eb6818f
Aladdin <g.tabroid@gmail.com>
2022-11-18 04:52:50 +0300

    MarkdownViewer component

--- app/services/api/api.types.ts
+++
@@ -1,8 +0,0 @@
-/**
- * The options used to configure apisauce.
- */
-export interface ApiConfig {
-  apiUrl: string
-  authToken: string
-  timeout: number
-}
--- app/services/api/index.ts
+++ app/services/api/index.ts
@@ -1,2 +1,3 @@
-export * from './api';
-export * from './api.types';
+import api from './api';
+
+export default api;
